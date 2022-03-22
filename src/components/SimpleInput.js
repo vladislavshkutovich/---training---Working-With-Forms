bae2c573db934679786a1e813d86b792db8ada17
Vladislav Shkutovich <vladislav.shkutovich@gmail.com>
2022-03-22 15:15:45 +0300

    Validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,15 +1,79 @@
+import { useRef, useState, useEffect } from 'react';
+
 const SimpleInput = (props) => {
-  return (
-    <form>
-      <div className='form-control'>
-        <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
-      </div>
-      <div className="form-actions">
-        <button>Submit</button>
-      </div>
-    </form>
-  );
+	const nameInputRef = useRef();
+	const [enteredName, setEnteredName] = useState('');
+	const [enteredNameIsValid, setEnteredNameIsValid] = useState(false);
+	const [enteredNameTouched, setEnteredNameTouched] = useState(false);
+
+	useEffect(() => {
+		if (enteredNameIsValid) {
+			console.log('Name input is valid!');
+		}
+	}, [enteredNameIsValid]);
+
+	const nameInputChangeHandler = (event) => {
+		setEnteredName(event.target.value);
+	};
+
+	const nameInputBlurHandler = (event) => {
+		setEnteredNameTouched(true);
+
+		if (enteredName.trim() === '') {
+			setEnteredNameIsValid(false);
+			return;
+		}
+	};
+
+	const formSubmissionHandler = (event) => {
+		event.preventDefault();
+
+		setEnteredNameTouched(true);
+
+		if (enteredName.trim() === '') {
+			setEnteredNameIsValid(false);
+			return;
+		}
+
+		setEnteredNameIsValid(true);
+
+		// useState()
+		console.log(enteredName);
+
+		// useRef()
+		const enteredValue = nameInputRef.current.value;
+		console.log(enteredValue);
+
+		setEnteredName('');
+	};
+
+	const nameInputIsInvalid = !enteredNameIsValid && enteredNameTouched;
+
+	const nameInputClasses = !nameInputIsInvalid
+		? 'form-control'
+		: 'form-control invalid';
+
+	return (
+		<form onSubmit={formSubmissionHandler}>
+			<div className={nameInputClasses}>
+				<label htmlFor="name">Your Name</label>
+				<input
+					ref={nameInputRef}
+					type="text"
+					id="name"
+					onChange={nameInputChangeHandler}
+					onBlur={nameInputBlurHandler}
+					value={enteredName}
+				/>
+				{nameInputIsInvalid && (
+					<p className="error-text">Name must not be empty.</p>
+				)}
+			</div>
+			<div className="form-actions">
+				<button>Submit</button>
+			</div>
+		</form>
+	);
 };
 
 export default SimpleInput;
